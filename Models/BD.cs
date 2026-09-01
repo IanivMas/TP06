40c8d4d1af93fa7dfa6d45ea8a8332b38a36cde6
IANIV MAS <49836040@ort>
2026-09-01 09:00:57 -0300

    falta el controller y las salas

--- Models/BD.cs
+++ Models/BD.cs
@@ -1,8 +1,34 @@
 namespace TP06.Models;
+
 using Microsoft.Data.SqlClient;
 using Dapper;
 public class BD
 {
- private string conexion = @"Server=localhost;DataBase=TP06; Integrated Security=True; TrustServerCertificate=True;";
- 
+    private string conexion = @"Server=localhost;DataBase=TP06; Integrated Security=True; TrustServerCertificate=True;";
+    public void agregarJugador(Jugador j)
+    {
+        string query = "INSERT INTO Jugador (Nombre,Apellido,Usuario,Clave,Email,Vidas,Progreso) VALUES (@Nombre,@Apellido,@Usuario,@Clave,@Tipo,@Email,@Vidas,@Progreso)";
+        using (SqlConnection connection = new SqlConnection(conexion))
+        {
+            connection.Execute(query, new { Nombre = j.Nombre, Apellido = j.Apellido, Usuario = j.Usuario, Clave = j.Clave, Email = j.Email, Vidas = j.Vidas, Progreso = j.Progreso });
+        }
+    }
+    public Sala ObtenerSala(int id)
+    {
+        string query = "SELECT * FROM Sala WHERE SalaId = @id";
+
+        using (SqlConnection connection = new SqlConnection(conexion))
+        {
+            return connection.QueryFirstOrDefault<Sala>( query,new { id });
+        }
+    }
+    public List<Sala> ObtenerSalas()
+    {
+        string query = "SELECT * FROM Sala ORDER BY Nivel";
+
+        using (SqlConnection connection = new SqlConnection(conexion))
+        {
+            return connection.Query<Sala>(query).ToList();
+        }
+    }
 }
