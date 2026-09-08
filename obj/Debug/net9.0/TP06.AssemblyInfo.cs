cd6b57e171af7d1498b58a1c03ee7a09c6165d93
IANIV MAS <49836040@ort>
2026-09-08 11:23:52 -0300

    falta la  comprobacion de las paginas

--- obj/Debug/net9.0/TP06.AssemblyInfo.cs
+++ obj/Debug/net9.0/TP06.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP06")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e210ffed27ca658aca2373c53d673dd0793070c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaf16034c2867fd40a0bed461bc66e274a0f9cf4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP06")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP06")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
